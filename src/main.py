8b6ea5b2ef0b68907c5a336192a10c98493cd678
Collin Abidi <cba15@pitt.edu>
2019-04-03 16:36:56 -0400

    made working copy that uses lenet

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ img_rows, img_cols = 32, 32 # Resolution of inputs
 channel = 3 # rgb
 num_classes = 10 # cifar 100
 
-"""
 # lenet model for testing
 conv1 = {'name':'conv1','type':'Convolution2D','border_mode':'same','nb_filter':20,'nb_row':5,'nb_col':5,'input_shape':(img_rows,img_cols,channel)}
 activation1 = {'name':'activation1','type':'Activation','activation':'relu'}
@@ -25,8 +24,8 @@ activation3 = {'name':'activation3','type':'Activation','activation':'relu'}
 dense2 = {'name':'dense2','type':'Dense','output_dim':num_classes}
 activation4 = {'name':'output','type':'Activation','activation':'softmax'}
 p = [conv1,activation1,max1,conv2,activation2,max2,flatten1,dense1,activation3,dense2,activation4]
-"""
 
+"""
 #stage 1
 conv1_1 = {'name':'conv1_1','type':'Convolution2D','border_mode':'same','nb_filter':64,'nb_row':224,'nb_col':224,'input_shape':(img_rows,img_cols,channel)}
 activation1_1 = {'name':'activation1_1','type':'Activation','activation':'relu'}
@@ -55,7 +54,7 @@ activation4_2 = {'name':'activation4_2','type':'Activation','activation':'relu'}
 conv4_3 = {'name':'conv4_3','type':'Convolution2D','border_mode':'same','nb_filter':512,'nb_row':28,'nb_col':28}
 activation4_3 = {'name':'activation4_3','type':'Activation','activation':'relu'}
 max4 = {'name':'max4','type':'MaxPooling2D','pool_size':(2,2),'strides':(2,2)}
-#stage 5"""
+#stage 5
 conv5_1 = {'name':'conv5_1','type':'Convolution2D','border_mode':'same','nb_filter':512,'nb_row': 14,'nb_col':14}
 activation5_1 = {'name':'activation5_1','type':'Activation','activation':'relu'}
 conv5_2 = {'name':'conv5_2','type':'Convolution2D','border_mode':'same','nb_filter':512,'nb_row':14,'nb_col':14}
@@ -76,19 +75,20 @@ p = [
 	conv5_1,activation5_1,conv5_2,activation5_2,conv5_3,activation5_3,max5,
 	flatten1,dense3,activation_dense3
 	]
-
+"""
 
 
 pop = Population(p,size=1)
 
-batch_size = 10
+batch_size = 16
 nb_epoch = 15
-train_num = 100
-valid_num = 100
+train_num = 1000
+valid_num = 1000
 X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols, nb_train_samples=train_num,nb_valid_samples=valid_num)
 augment_ratio = 2
-"""
 X_train, Y_train = augment_data(X_train,Y_train,batch_size,augment_ratio)
+"""
+# data memory management
 X_train = da.from_array(np.asarray(X_train), chunks=(int(train_num/augment_ratio))*augment_ratio)
 Y_train = da.from_array(np.asarray(Y_train), chunks=(int(train_num/augment_ratio))*augment_ratio)
 X_valid = da.from_array(np.asarray(X_valid), chunks=(int(valid_num/augment_ratio))*augment_ratio)
